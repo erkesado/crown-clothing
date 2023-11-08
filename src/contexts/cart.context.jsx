03e43c4e97057382c9b4c895b69402b97f30e307
erkesado <sadoo33@gmail.com>
2023-11-08 10:13:09 +0800

    Sectuib 13: Reducers

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,20 +1,20 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer } from "react";
+
+import { createAction } from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingItem = cartItems.find((item) => item.id === productToAdd.id);
 
   if (existingItem) {
     existingItem.quantity += 1;
-    return [...cartItems];
-  }
-
-  return [
-    ...cartItems,
-    {
+  } else {
+    cartItems.push({
       ...productToAdd,
       quantity: 1,
-    },
-  ];
+    });
+  }
+
+  return cartItems;
 };
 
 const removeCartItem = (cartItems, cartItemToRemove) => {
@@ -26,7 +26,7 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     return cartItems.filter((item) => item.id !== cartItemToRemove.id);
 
   existingItem.quantity -= 1;
-  return [...cartItems];
+  return cartItems;
 };
 
 const clearCartItem = (cartItems, cartItemToClear) => {
@@ -37,45 +37,82 @@ export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   addItemToCart: () => {},
-  cartItems: {},
+  cartItems: [],
   cartCount: 0,
   cartTotal: 0,
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
 });
 
+export const CART_ACTION_TYPES = {
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+  SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+};
+
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload,
+      };
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
+    default:
+      throw new Error(`Unhandled type of${type} in cartReducer`);
+  }
+};
+
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-  const [cartTotal, setCartTotal] = useState(0);
+  const [state, dispatch] = useReducer(cartReducer, INITIAL_STATE);
+  const { isCartOpen, cartItems, cartCount, cartTotal } = state;
 
-  useEffect(() => {
-    const newCartCount = cartItems.reduce(
-      (count, cartItem) => count + cartItem.quantity,
-      0
-    );
-    setCartCount(newCartCount);
-  }, [cartItems]);
+  const updateCartItemsReducer = (newCartItems) => {
+    let newCartCount = 0,
+      newCartTotal = 0;
+    for (const item of newCartItems) {
+      newCartCount += item.quantity;
+      newCartTotal += item.price * item.quantity;
+    }
 
-  useEffect(() => {
-    const newCartTotal = cartItems.reduce(
-      (total, cartItem) => total + cartItem.quantity * cartItem.price,
-      0
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+        cartItems: newCartItems,
+        cartCount: newCartCount,
+        cartTotal: newCartTotal,
+      })
     );
-    setCartTotal(newCartTotal);
-  }, [cartItems]);
+  };
 
   const addItemToCart = (productToAdd) => {
-    setCartItems(addCartItem(cartItems, productToAdd));
+    const newCartItems = addCartItem(cartItems, productToAdd);
+    updateCartItemsReducer(newCartItems);
   };
 
   const removeItemFromCart = (cartItemToRemove) => {
-    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+    const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+    updateCartItemsReducer(newCartItems);
   };
 
   const clearItemFromCart = (cartItemToClear) => {
-    setCartItems(clearCartItem(cartItems, cartItemToClear));
+    const newCartItems = clearCartItem(cartItems, cartItemToClear);
+    updateCartItemsReducer(newCartItems);
+  };
+
+  const setIsCartOpen = (value) => {
+    dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, value));
   };
 
   const value = {
