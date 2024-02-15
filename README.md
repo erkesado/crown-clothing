039c62963269ea7134118678d3eddb67895c5b9d
erkesado <sadoo33@gmail.com>
2024-02-15 16:15:47 +0800

    setup gh-pages

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # crown-clothing
-My repo for the ZTM React Developer course: https://www.udemy.com/course/complete-react-developer-zero-to-mastery/
+
+My repo for the Complete React Develop course by ZTM Academy: https://www.udemy.com/course/complete-react-developer-zero-to-mastery/
 
 This repo covers the Crown Clothing project in the course.
-
-
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -8,16 +8,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import { store, persistor } from "./store/store";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </PersistGate>
   </Provider>
   // </React.StrictMode>
